090fcae025445f53771a8f371d604957c3473ebc
cognifex <cognifaktur@gmail.com>
2025-10-29 11:29:05 +0100

    fix: allow notionusercontent images

--- next.config.js
+++ next.config.js
@@ -21,6 +21,7 @@ export default withBundleAnalyzer({
         hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com'
       },
       { protocol: 'https', hostname: 'prod-files-secure.s3.amazonaws.com' },
+      { protocol: 'https', hostname: 'img.notionusercontent.com' },
       { protocol: 'https', hostname: 's3.us-west-2.amazonaws.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'abs.twimg.com' },
